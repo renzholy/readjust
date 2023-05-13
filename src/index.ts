ca24dca52b7da01a215e93a2a40dd23683540f44
Aliez <renzholy@gmail.com>
2023-05-13 18:46:14 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import {
   example_changes,
   example_toc,
 } from './template.js'
-import { render_html } from './xhtml.js'
 
 const zip = new JSZip()
 
@@ -16,22 +15,24 @@ zip.file('mimetype', 'application/epub+zip')
 
 const meta = zip.folder('META-INF')
 
-meta?.file('container.xml', meta_inf_container)
+if (meta) {
+  meta.file('container.xml', meta_inf_container)
+}
 
 const epub = zip.folder('EPUB')
 
-epub?.file('package.opf', epub_package_opf)
-
-epub?.file(
-  'style.css',
-  await (await fetch('https://cdn.tailwindcss.com?plugins=typography')).text(),
-)
-
-epub?.file('cover.jpg', epub_img_cover_image)
-
-epub?.file('epub30-nav.xhtml', render_html(example_toc))
-
-epub?.file('epub30-changes.xhtml', render_html(example_changes))
+if (epub) {
+  epub.file('package.opf', epub_package_opf)
+  epub.file(
+    'style.css',
+    await (
+      await fetch('https://cdn.tailwindcss.com?plugins=typography')
+    ).text(),
+  )
+  epub.file('cover.jpg', epub_img_cover_image)
+  epub.file('epub30-nav.xhtml', example_toc)
+  epub.file('epub30-changes.xhtml', example_changes)
+}
 
 zip.generateAsync({ type: 'nodebuffer' }).then(function (content) {
   fs.writeFile('output.epub', content)

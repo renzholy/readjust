1213103257d984820c2c936d62fd0e9aca5b53fe
Aliez <renzholy@gmail.com>
2023-05-19 21:43:19 +0900

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -4,7 +4,37 @@ export default {
     env: Env,
     ctx: ExecutionContext,
   ): Promise<Response> {
-    const url = new URL(request.url)
-    return new Response('', { headers: { 'Content-Type': 'text/html' } })
+    const url = new URL(request.url).searchParams.get('url')
+    if (!url) {
+      return new Response(null, { status: 400 })
+    }
+    const response = await fetch(url)
+    return new HTMLRewriter()
+      .onDocument({
+        doctype(doctype) {
+          console.log('doctype', doctype)
+        },
+        comments(comment) {
+          console.log('comment', comment)
+        },
+        text(text) {
+          console.log('text', text)
+        },
+        end(end) {
+          console.log('end', end)
+        },
+      })
+      .on('*', {
+        element(element) {
+          console.log('element', element)
+        },
+        comments(comment) {
+          console.log('comment', comment)
+        },
+        text(text) {
+          console.log('text', text)
+        },
+      })
+      .transform(response)
   },
 }
